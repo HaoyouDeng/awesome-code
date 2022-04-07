2bc2da8c4917cc4146ee699f3416c49654853e5f
Haoyou Deng <deng_haoyou@qq.com>
2022-04-07 11:25:47 +0800

    tensorboard add_scalars

--- tensorboard_demo.py
+++ tensorboard_demo.py
@@ -3,10 +3,16 @@ import math
 
 writer = SummaryWriter(log_dir='./log') # log_dir = './path/to/log'
 
+x = []
 for epoch in range(100):
     mAP = -math.log(epoch+1)
+
+    x = [mAP, 2*mAP, -mAP]
+
     writer.add_scalar('mAP', mAP, epoch)
     writer.add_scalar('mAP2', mAP, epoch)
+    writer.add_scalars('mAP3', {'aa':x[0],'bb':x[1]}, epoch)
+    print(epoch)
 
 '''
 tensorboard --logdir=./path/to/the/folder --port 8123   # 8123 is an example

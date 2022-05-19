c490bb9b6db65b0c1a329da681ca3500e72205af
Haoyou Deng <deng_haoyou@qq.com>
2022-05-19 10:37:44 +0800

    add loguru

---
+++ GradientReversal.py
@@ -0,0 +1,48 @@
+import numpy as np
+import torch
+from torch.autograd import Function
+
+class GradientReversalFunction(Function):
+    """
+    Gradient Reversal Layer from:
+    Unsupervised Domain Adaptation by Backpropagation (Ganin & Lempitsky, 2015)
+
+    Forward pass is the identity function. In the backward pass,
+    the upstream gradients are multiplied by -lambda (i.e. gradient is reversed)
+    """
+
+    @staticmethod
+    def forward(ctx, x, lambda_):
+        ctx.lambda_ = lambda_
+        return x.clone()
+
+    @staticmethod
+    def backward(ctx, grads):
+        lambda_ = ctx.lambda_
+        lambda_ = grads.new_tensor(lambda_)
+        dx = -lambda_ * grads
+        return dx, None
+
+
+class GradientReversal(torch.nn.Module):
+    def __init__(self, lambda_=1):
+        super(GradientReversal, self).__init__()
+        self.lambda_ = lambda_
+
+    def forward(self, x):
+        return GradientReversalFunction.apply(x, self.lambda_)
+
+
+'''
+https://zhuanlan.zhihu.com/p/263827804
+'''
+class GradReverse(Function):
+    
+    @ staticmethod
+    def forward(ctx, x, lambd, **kwargs: None):
+        ctx.lambd = lambd
+        return x.view_as(x)
+
+    @staticmethod
+    def backward(ctx, *grad_output):
+        return grad_output[0] * -ctx.lambd, None
